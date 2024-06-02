23f810fd75e7f528372d7728bdf7de3e34f9299e
d1marseee <dimachichugaa@gmail.com>
2024-06-02 12:57:38 +0200

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,46 @@
+const user = {
+  name: "Дмитро",
+  surname: "Чичуга",
+  userPhoto: "my-photo.jpg",
+  qrQode: "qr-code.svg",
+  dreams: [
+    { src: "Porsche.jpg", alt: "Авто моєї мрії" },
+    { src: "Jadon.jpg", alt: "Мрія стати футблістом" },
+    { src: "voyage.jpg", alt: "Подорож до Марсу" },
+  ],
+};
+
+// arr це середня місячна платня по рокам
+const arr = [
+  500,
+  700,
+  800,
+  1000,
+  1200,
+  1500,
+  1800,
+  2100,
+  2400,
+  2700,
+  3000,
+  3300,
+];
+
+const necessaryExpenses = 800;
+
+const todo = [
+  { month: "Червень", skill: "JavaScript" },
+  { month: "Липень", skill: "Java" },
+  { month: "Серпень", skill: "Python" },
+  { month: "Вересень", skill: "Самостійність" },
+  { month: "Жовтень", skill: "Гнучкість" },
+  { month: "Листопад", skill: "Емоційний інтелект" },
+  { month: "Грудень", skill: "Креативність" },
+  { month: "Січень", skill: "Лідерство" },
+  { month: "Лютий", skill: "Нетворкінг" },
+  { month: "Березень", skill: "Особистий бренд" },
+  { month: "Квітень", skill: "Управління фінансами" },
+  { month: "Травень", skill: "Публічні виступи" },
+];
+
+export { user, arr, necessaryExpenses, todo };
